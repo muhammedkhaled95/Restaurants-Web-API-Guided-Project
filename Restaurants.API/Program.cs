a0b1483b29880b5d79350660773131ba5698dbf3
muhammedkhaled95 <muhammedkhaled111@gmail.com>
2025-03-07 21:34:27 +0200

    Creating an integration test for testing the endpoints without authorization needed using WebApplicationFactory class.

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -97,3 +97,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+public partial class Program { }
---
+++ Restaurants.APITests/Controllers/RestaurantsControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Restaurants.API.Controllers.Tests;
+
+public class RestaurantsControllerTests: IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _applicationFactory;
+    public RestaurantsControllerTests(WebApplicationFactory<Program> applicationFactory)
+    {
+        _applicationFactory = applicationFactory;
+    }
+
+    [Fact()]
+    public async void GetAllTest_ForValidRequest_ShouldReturnStatusCode200OK()
+    {
+        // Arrange
+        var client = _applicationFactory.CreateClient();
+
+        // Act
+        var result = await client.GetAsync("api/restaurants?pageNumber=1&pageSize=10");
+        
+        // Assert
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+
+    [Fact()]
+    public async void GetAllTest_ForInvalidRequest_ShouldReturnStatusCode400BadRequest()
+    {
+        // Arrange
+        var client = _applicationFactory.CreateClient();
+
+        // Act
+        var result = await client.GetAsync("api/restaurants");
+
+        // Assert
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+}
