8de13eacf2a4ef63d68ee85f07161110c4343388
muhammedkhaled95 <muhammedkhaled111@gmail.com>
2025-03-07 22:26:13 +0200

    Creating Integration tests for endpoints with authorization taken into account by having fake authorization policy giving the access to any endpoint by bypassing its auth and authz requirements.

--- Restaurants.APITests/Controllers/RestaurantsControllerTests.cs
+++ Restaurants.APITests/Controllers/RestaurantsControllerTests.cs
@@ -1,16 +1,89 @@
 ﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using Restaurants.API.Tests;
+using Restaurants.Application.Restaurants.DTOs;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Seeders;
+using System.Net.Http.Json;
+using System.Net;
 using Xunit;
 
 namespace Restaurants.API.Controllers.Tests;
 
-public class RestaurantsControllerTests: IClassFixture<WebApplicationFactory<Program>>
+public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _applicationFactory;
-    public RestaurantsControllerTests(WebApplicationFactory<Program> applicationFactory)
+    private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+    private readonly Mock<IRestaurantSeeder> _restaurantsSeederMock = new();
+    public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
     {
-        _applicationFactory = applicationFactory;
+        _applicationFactory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository),
+                                            _ => _restaurantsRepositoryMock.Object));
+
+
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantSeeder),
+                                            _ => _restaurantsSeederMock.Object));
+            });
+        });
     }
+
+    [Fact()]
+    public async void GetByIdTest_ForNonExistingId_ShouldReturnStatusCode404NotFound()
+    {
+        // Arrange
+        var id = 1123;
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync((Restaurant?)null);
+        
+        var client = _applicationFactory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"api/restaurants/{id}");
+        
+        // Assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task GetById_ForExistingId_ShouldReturn200Ok()
+    {
+        // arrange
+
+        var id = 99;
+
+        var restaurant = new Restaurant()
+        {
+            Id = id,
+            Name = "Test",
+            Description = "Test description"
+        };
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(restaurant);
+
+        var client = _applicationFactory.CreateClient();
+
+        // act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+        var restaurantDto = await response.Content.ReadFromJsonAsync<RestaurantDto>();
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        restaurantDto.Should().NotBeNull();
+        restaurantDto.Name.Should().Be("Test");
+        restaurantDto.Description.Should().Be("Test description");
+    }
+
 
     [Fact()]
     public async void GetAllTest_ForValidRequest_ShouldReturnStatusCode200OK()
@@ -20,7 +93,7 @@ public class RestaurantsControllerTests: IClassFixture<WebApplicationFactory<Pro
 
         // Act
         var result = await client.GetAsync("api/restaurants?pageNumber=1&pageSize=10");
-        
+
         // Assert
         result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
     }
